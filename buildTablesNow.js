4b5cb8947f7669c681d980d47b83cd486a8a71fc
Pomax <pomax@nihongoresources.com>
2014-02-07 09:46:47 -0500

    .

--- buildTablesNow.js
+++ buildTablesNow.js
@@ -171,19 +171,19 @@
   });
 
   // create stylesheet that uses this font
-/*
+
   var mime = "font/opentype";
   var dataurl = "data:" + mime + ";base64," + btoa(font.otf.map(asChars).join(''));
   var fontface = "@font-face {\n  font-family: 'custom font';\n  src: url('" +dataurl+ "') format('opentype');\n}";
   var sheet = document.createElement("style");
   sheet.innerHTML = fontface;
   document.head.appendChild(sheet);
-*/
+/*
   var mime = "application/font-woff";
   var dataurl = "data:" + mime + ";base64," + btoa(font.woff.map(asChars).join(''));
   var fontface = "@font-face {\n  font-family: 'custom font';\n  src: url('" +dataurl+ "') format('woff');\n}";
   var sheet = document.createElement("style");
   sheet.innerHTML = fontface;
   document.head.appendChild(sheet);
-
+*/
 }());
